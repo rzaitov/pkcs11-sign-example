ade0a7cbc1696c50421ce3b48a1323cdabdd356e
rzaitov <r-zaitov@yandex.ru>
2023-06-15 16:28:01 +0200

    Проверяю подпись в цикле

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,12 @@ use cryptoki::{
         Slot,
     },
 };
-use rand::rngs::OsRng;
 use rsa::{BigUint, PaddingScheme, PublicKey};
-use std::convert::TryFrom;
-use std::path::PathBuf;
+use std::{
+    convert::TryFrom,
+    path::PathBuf,
+    thread,
+};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -105,34 +107,19 @@ fn main() -> Result<()> {
     let pubexp = extract_public_exponent(&session, enc_objects[0])?;
 
     // Use the RustCrypto RSA crate to establish the public key locally
-    let mut rng = OsRng;
     let pubkey = rsa::RSAPublicKey::new(modulus, pubexp)?;
 
     // Encrypt using the public key from the device, specifying PKCS1 1.5 padding
     let message = "This is my secret".as_bytes().to_vec();
-    let output = pubkey.encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), &message)?;
-    assert_ne!(output, message);
 
-    // Now extract the plaintext bytes, decrypting via the User PIN authenticated Session to the
-    // Nitrokey HSM
-    let plaintext = session.decrypt(
-        &Mechanism::RsaPkcs,
-        dec_objects[0],
-        &output,
-    )?;
+    loop {
+        let signature = session.sign(&Mechanism::Sha256RsaPkcs, private_key, &message).unwrap();
+        let mut hasher = Sha256::new();
+        hasher.update(&message);
+        let hash = hasher.finalize();
+        pubkey.verify(PaddingScheme::PKCS1v15Sign { hash: Some(rsa::Hash::SHA2_256)}, &hash, &signature)?;
+        println!("signature valid");
 
-    let signature = session.sign(&Mechanism::Sha256RsaPkcs, private_key, &message).unwrap();
-    let mut hasher = Sha256::new();
-    hasher.update(&message);
-    let hash = hasher.finalize();
-    pubkey.verify(PaddingScheme::PKCS1v15Sign { hash: Some(rsa::Hash::SHA2_256)}, &hash, &signature)?;
-    println!("signature valid");
-
-    // Basic final checks
-    assert_eq!(message, plaintext);
-
-    // Proof for the human
-    let plaintext_str = String::from_utf8(plaintext)?;
-    println!("Decrypted secret: '{}'", plaintext_str);
-    Ok(())
+        thread::sleep(std::time::Duration::from_secs(10));
+    }
 }
